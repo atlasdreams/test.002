635d61a2894640a37a78bd027a15c99f24c637ed
Manuel Raimondi <manuel.raimondi@ECRMITALIA.local>
2016-09-29 17:35:51 +0200

    InstalledWebPack

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,3 +3,4 @@
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/underscore/index.d.ts" />
+/// <reference path="globals/webpack/index.d.ts" />
